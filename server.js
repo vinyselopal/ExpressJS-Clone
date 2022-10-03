7cac13efb4fe15d3b34ee309bc8274cbac044dea
viny <viny0698@gmail.com>
2022-10-03 17:06:27 +0530

    added get and send methods. Not working.

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const express = require('./lib/express.js')
 const {PORT} = require('./config.js')
 
-const server = http.createServer((req, res) => {
-    fs.readFile(path.resolve(__dirname, 'index.html'), (err, data) => {
-        res.setHeader('Content-Type', 'text/html')
-        if (err) {
-            res.writeHead(500)
-            console.log(err)
-            return res.end('Some error occured')
-        }
-        res.writeHead(200)
-        return res.end(data)
-    })
+const app = express()
+app.get('/', (req, res) => {
+    res.send()
 })
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT)
