13d8324b446c0fae05ed38c38010a3de64229649
viny <viny0698@gmail.com>
2022-10-06 13:10:46 +0530

    routing, request object and response object functionalities implemented

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ const express = require('./lib/express.js')
 const {PORT} = require('./config.js')
 
 const app = express()
-app.get('/', (req, res) => {
-    res.send()
+app.get('/a', (req, res) => {
+    // const { id } = req.params
+    const body = {
+        "response": "successful"
+    }
+    res.json(body)
 })
+
+app.get('/a/b', (req, res) => {
+    // const { id } = req.params
+    const body = 'successful response'
+    res.send(body)
+})
+
+app.get('/', (req, res) => {
+    // const { id } = req.params
+    const body = 'successful response'
+    res.send(body)
+})
+
+app.get('///', (req, res) => {
+    // const { id } = req.params
+    const body = 'successful response'
+    res.send(body)
+})
+
 app.listen(PORT)
