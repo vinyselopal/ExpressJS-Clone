5a18e38ded8d0968f278eadeb755a33e9a98ab08
viny <viny0698@gmail.com>
2022-10-07 14:21:31 +0530

    app.use() and put, post, etc method routes implemented

--- server.js
+++ server.js
@@ -2,25 +2,40 @@ const express = require('./lib/express.js')
 const {PORT} = require('./config.js')
 
 const app = express()
-app.get('/a', (req, res) => {
+
+app.post('/', (req, res) => {
+    // const { id } = req.params
+    const body = 'successful post response'
+    res.send(body)
+})
+
+app.use('/', (req, res, next) => {
+    res.body = 4
+    next()
+})
+
+app.use('/', (req, res, next) => {
+    res.body = JSON.stringify(res.body) // error: invalid string length, also res.body is a huge string of /
+    const body = res.body
+    res.send(body)
+    next()
+})
+
+app.get('/a', (req, res, next) => {
     // const { id } = req.params
     const body = {
         "response": "successful"
     }
-    res.json(body)
+    next()
 })
 
-app.get('/a/b', (req, res) => {
+app.get('/a', (req, res) => {
     // const { id } = req.params
     const body = 'successful response'
     res.send(body)
 })
 
-app.get('/', (req, res) => {
-    // const { id } = req.params
-    const body = 'successful response'
-    res.send(body)
-})
+
 
 app.get('///', (req, res) => {
     // const { id } = req.params
